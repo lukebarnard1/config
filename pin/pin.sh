3ff9399ef263f492c0fbd163f22d4e740bbb9b42
lukebarnard <luke.barnard99@gmail.com>
2019-11-02 19:19:24 +0000

    Fix whitespace in pin

--- pin/pin.sh
+++ pin/pin.sh
@@ -9,30 +9,30 @@ if [[ -e $PIN_DIR/last ]]; then
 fi
 
 pin () {
-	local firstArg=$1
-	local choice=""
+  local firstArg=$1
+  local choice=""
 
-	if [[ $firstArg == "here" ]]; then
-		p=$(pwd)
-		echo ${p##$HOME} >> $PIN_DIR/ls
-		cat $PIN_DIR/ls | sort -u > $PIN_DIR/ls^
-		mv $PIN_DIR/ls^ $PIN_DIR/ls
-	elif [[ $firstArg == "help" ]]; then
+  if [[ $firstArg == "here" ]]; then
+    p=$(pwd)
+    echo ${p##$HOME} >> $PIN_DIR/ls
+    cat $PIN_DIR/ls | sort -u > $PIN_DIR/ls^
+    mv $PIN_DIR/ls^ $PIN_DIR/ls
+  elif [[ $firstArg == "help" ]]; then
     less $CONFIG_DEV_DIR/pin/help.txt
-	elif [[ -z $firstArg ]]; then
+  elif [[ -z $firstArg ]]; then
     if [[ $(fzf --version &>/dev/null;echo $?) -ne 0  ]]; then
       echo 'To use fuzzy search, please install fzf:'
       echo ' git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf'
       echo ' ~/.fzf/install'
       return 1
     fi;
-		choice=$(cat $PIN_DIR/ls | fzf)
-	else
-		choice=$(grep -m1 -e "$firstArg" $PIN_DIR/ls)
-	fi
+    choice=$(cat $PIN_DIR/ls | fzf)
+  else
+    choice=$(grep -m1 -e "$firstArg" $PIN_DIR/ls)
+  fi
 
-	if [[ -n $choice ]]; then
-		cd $HOME$choice
-		echo $HOME$choice > $PIN_DIR/last
-	fi
+  if [[ -n $choice ]]; then
+    cd $HOME$choice
+    echo $HOME$choice > $PIN_DIR/last
+  fi
 }
